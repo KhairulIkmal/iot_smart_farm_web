ae576a3463f06dfcacca75fbc9249819a86e87df
khx.mal <ikmal03411@gmail.com>
2026-05-29 01:32:44 +0800

    fix: remove unnecessary SPA redirect script and update base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/iot_smart_farm_web/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
